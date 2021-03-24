d41646300a53a7b3e86375ffaac0a3508ae3de6e
NathanCong <congyunan@xdf.cn>
2021-03-24 14:39:05 +0800

    提交背景

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="fonts background">
+    <!-- <router-view /> -->
   </div>
 </template>
 
 <style lang="less">
 #app {
+  width: 100%;
+  height: 100vh;
+}
+
+.fonts {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -17,16 +18,10 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.background {
+  background-color: #ccc;
+  background-image: url('~@/assets/bg.png');
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
 }
 </style>

e414bce9245ca4e6289cdaaba9db6b0d0775d01f
NathanCong <congyunan@xdf.cn>
2021-03-26 06:43:58 +0800

    提交 Kyc2 页面

--- src/views/Kyc2.vue
+++ src/views/Kyc2.vue
@@ -3,38 +3,50 @@
     <GoBackLink class="go-back" />
     <Layout>
       <template #layout-left-content>
-        <Card bgColor="#c2723a" :opacity="0.65">
-          <div class="kyc2-info">
-            <p class="kyc2-info-logo">LOGO</p>
-            <p class="kyc2-info-name">Bitwire Pay</p>
-            <p class="kyc2-info-desc">The best payment gateway&payouts for India</p>
-          </div>
-        </Card>
+        <div class="kyc2-pan-card">
+          <header class="kyc2-pan-card-header">Verify Pan Card</header>
+          <main class="kyc2-pan-card-display">
+            <i class="kyc2-pan-card-icon"></i>
+            <p class="kyc2-pan-card-text">Pan card image</p>
+          </main>
+          <footer class="kyc2-pan-card-buttons">
+            <i class="kyc2-pan-card-button photo"></i>
+            <i class="kyc2-pan-card-button camera"></i>
+          </footer>
+        </div>
       </template>
       <template #layout-right-content>
         <div class="kyc2-wrapper">
-          <Title class="kyc2-title" value="Sign In" />
+          <Title class="kyc2-title" value="Pan" />
           <section class="kyc2-form-wrapper">
-            <Card :opacity="0.8">
+            <Card :opacity="0.8" class="kyc2-form-card">
               <div class="kyc2-form">
                 <section class="kyc2-form-item">
-                  <Label value="Email" />
+                  <Label value="Name" />
                   <Input
                     class="kyc2-form-input"
                     type="text"
-                    placeholder="Enter your username or Email"
+                    placeholder="Please input your name"
                   />
                 </section>
                 <section class="kyc2-form-item">
-                  <Label value="Password" />
+                  <Label value="Pan Number" />
                   <Input
                     class="kyc2-form-input"
                     type="password"
-                    placeholder="Enter your Password"
+                    placeholder="Input Pan cam number"
                   />
                 </section>
                 <section class="kyc2-form-item">
-                  <Button class="kyc2-form-button">Sign In Securely</Button>
+                  <Label value="Dale of birth" />
+                  <Select class="kyc2-form-select" />
+                </section>
+                <section class="kyc2-form-item">
+                  <Label value="Province" />
+                  <Select class="kyc2-form-select" />
+                </section>
+                <section class="kyc2-form-item">
+                  <Button class="kyc2-form-button">SUBMIT</Button>
                 </section>
               </div>
             </Card>
@@ -53,6 +65,7 @@ import Label from '@/components/common/Label.vue';
 import Input from '@/components/common/Input.vue';
 import Button from '@/components/common/Button.vue';
 import Title from '@/components/common/Title.vue';
+import Select from '@/components/common/Select.vue';
 
 export default {
   name: 'Kyc2',
@@ -64,6 +77,7 @@ export default {
     Input,
     Button,
     Title,
+    Select,
   },
 };
 </script>
@@ -81,20 +95,61 @@ export default {
   left: 60px;
 }
 
-.kyc2-info {
-  color: #ffefdd;
-  font-weight: 400;
-  .kyc2-info-logo {
+.kyc2-pan-card {
+  padding: 70px 90px 0 90px;
+  .kyc2-pan-card-header {
+    background-image: url('~@/assets/card_logo.png');
+    background-repeat: no-repeat;
+    background-size: 54px 50px;
+    line-height: 50px;
     text-align: left;
-    font-size: 45px;
+    padding-left: 74px;
+    font-size: 43px;
+    color: #fff;
   }
-  .kyc2-info-name {
-    font-size: 56px;
-    padding-top: 115px;
-    padding-bottom: 115px;
+  .kyc2-pan-card-display {
+    width: 100%;
+    height: 400px;
+    position: relative;
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 35px;
+    margin-top: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .kyc2-pan-card-icon {
+      position: absolute;
+      top: 20px;
+      right: 20px;
+      width: 41px;
+      height: 41px;
+      background-image: url('~@/assets/warn_logo.png');
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+    .kyc2-pan-card-text {
+      font-size: 45px;
+      color: #fbc199;
+    }
   }
-  .kyc2-info-desc {
-    font-size: 46px;
+  .kyc2-pan-card-buttons {
+    margin-top: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .kyc2-pan-card-button {
+      margin: 0 30px;
+      width: 100px;
+      height: 100px;
+      background-repeat: no-repeat;
+      background-size: cover;
+      &.photo {
+        background-image: url('~@/assets/photo_logo.png');
+      }
+      &.camera {
+        background-image: url('~@/assets/camera_logo.png');
+      }
+    }
   }
 }
 
@@ -107,6 +162,9 @@ export default {
   .kyc2-title {
     padding-left: 56px;
   }
+  .kyc2-form-card {
+    padding-top: 20px;
+  }
   .kyc2-form-wrapper {
     width: 100%;
     height: 816px;
@@ -115,13 +173,14 @@ export default {
       .kyc2-form-item {
         padding-top: 20px;
       }
-      .kyc2-form-input {
+      .kyc2-form-input,
+      .kyc2-form-select {
         height: 70px;
         line-height: 70px;
         margin-top: 20px;
       }
       .kyc2-form-button {
-        margin-top: 43px;
+        margin-top: 20px;
       }
     }
   }

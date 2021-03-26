39166ade4835b501d24dae9daaa3d59aee8874a4
NathanCong <congyunan@xdf.cn>
2021-03-26 11:08:30 +0800

    添加图片压缩插件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   publicPath: '/',
+  chainWebpack: (config) => {
+    config.module.rule('images')
+      .test(/\.(gif|png|jpe?g|svg)$/i)
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({ bypassOnDebug: true })
+      .end();
+  },
 };

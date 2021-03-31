02fb40fc732f2cc6661d56e20eb8c8357af18b66
NathanCong <congyunan@xdf.cn>
2021-04-01 01:31:24 +0800

    提交 request 工具模块

---
+++ src/utils/request.js
@@ -0,0 +1,26 @@
+import Axios from 'axios';
+
+/**
+ * 全局 URL 设置
+ */
+Axios.defaults.baseURL = window.location.origin;
+
+/**
+ * Get 请求
+ * @param {String} url    请求地址
+ * @param {Object} params 请求参数
+ * @returns {Promise}     响应结果
+ */
+export function get(url, params) {
+  return Axios.get(url, { params });
+}
+
+/**
+ * Post 请求
+ * @param {String} url    请求地址
+ * @param {Object} params 请求参数
+ * @returns {Promise}     响应结果
+ */
+export function post(url, params) {
+  return Axios.post(url, params);
+}
